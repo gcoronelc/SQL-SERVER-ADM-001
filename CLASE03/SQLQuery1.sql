6909162e67db23590ad9370a5ddaff2e2e6e1e2f
Gustavo Coronel <gcoronelc@gmail.com>
2020-01-17 16:05:28 -0500

    Clase 03

--- CLASE03/SQLQuery1.sql
+++ CLASE03/SQLQuery1.sql
@@ -1,10 +1,29 @@
+use rh;
+go
 
 create table dbo.algo(
-id integer identity,
-dato varchar(100)
+  id integer identity,
+  dato varchar(100)
 );
 go
 
+begin transaction
+go
+
+INSERT INTO DBO.ALGO(DATO) VALUES('uni');
+INSERT INTO DBO.ALGO(DATO) VALUES('es');
+INSERT INTO DBO.ALGO(DATO) VALUES('uni');
+go
+
+SELECT * FROM DBO.ALGO;
+go
+
+commit transaction;
+go
+
+
+SELECT COUNT(*) FROM DBO.ALGO;
+go
 
 BEGIN TRANSACTION;
 GO
@@ -12,3 +31,5 @@ GO
 INSERT INTO DBO.ALGO(DATO) VALUES('ABC');
 
 SELECT COUNT(*) FROM DBO.ALGO;
+
+select * from dbo.algo;
