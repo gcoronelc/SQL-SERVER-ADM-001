757a61534f4c6824e31782e6b6bb14a912e879e9
Gustavo Coronel <gcoronelc@gmail.com>
2020-01-20 16:37:16 -0500

    Clase04

--- CLASE04/Lab06.sql
+++ CLASE04/Lab06.sql
@@ -14,7 +14,64 @@ go
 select * from master.dbo.sysdevices;
 go
 
+-- 6.3
+
+create table perudev..test(
+	id Int Identity Primary Key,
+	dato varchar(30)
+)
+go
+
+declare @k int
+set @k = 0
+while (@k < 100)
+begin
+begin tran
+	insert into perudev..test(dato) values('Backup de Base de Datos')
+	commit tran
+	set @k = @k + 1
+end
+go
+
+select * from perudev..test;
+
+-- 6.4
+
+backup database perudev
+to pd
+with
+	format,
+	name = 'BakBD',
+	description = 'Backup de la base de datos completa'
+go
+
+restore headeronly from pd
+go
 
 
+-- 6.5
+
+declare @k int
+set @k = 0
+while (@k < 100)
+begin
+begin tran
+	insert into perudev..test(dato) values('PERU CAMPEON')
+	commit tran
+	set @k = @k + 1
+end
+go
+
+backup database perudev
+to pd
+with
+	differential,
+	name = 'BakDif01',
+	description = 'Primer backup diferencial dela base de datos'
+go
+
+
+restore headeronly from pd
+go
 
 
