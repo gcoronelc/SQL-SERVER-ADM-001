2ef46b1052f4309243143e419bd9dfbc6892cbda
Gustavo Coronel <gcoronelc@gmail.com>
2020-01-24 17:52:43 -0500

    Clase 06

--- CLASE06/Work.sql
+++ CLASE06/Work.sql
@@ -24,7 +24,175 @@ group by c.IdCurso, c.NomCurso;
 go
 
 
-go
 
 select * from edutec.dbo.Tarifa;
 go
+
+
+USE EduTec; 
+go 
+
+SELECT * INTO dbo.Curso3 
+FROM dbo.Curso WHERE 1=2; 
+GO 
+
+
+-- Importación Masiva
+select * from dbo.Curso3;
+
+
+-- 3.2
+USE EduTec; 
+go 
+
+SELECT * INTO dbo.Curso4 
+FROM dbo.Curso WHERE 1=2; 
+GO 
+
+select * from dbo.Curso4; 
+go 
+
+bulk insert EduTec.dbo.Curso4 
+from 'g:\bcp\Curso_A.dat' 
+with (formatfile='g:\bcp\Curso.xml');
+go
+
+select * from dbo.Curso4; 
+go 
+
+
+-- 3.3
+select NomProfesor, ApeProfesor 
+into dbo.Profesor2 
+from dbo.Profesor 
+where 1=2;
+
+select * from dbo.Profesor2;
+
+bulk insert dbo.Profesor2 
+from 'g:\bcp\Profesores.txt' 
+with (fieldterminator='\t',rowterminator='\n'); 
+go 
+
+select * from dbo.Profesor2; 
+go
+
+
+
+-- 3.4
+
+SELECT * INTO dbo.Curso5 
+FROM dbo.Curso 
+WHERE 1=2; 
+GO 
+
+select * from dbo.Curso5; 
+go
+
+BULK INSERT dbo.Curso5 
+FROM 'g:\bcp\Curso-nat.Dat' 
+WITH (DATAFILETYPE='native'); 
+GO
+
+select * from dbo.Curso5; 
+go
+
+-- 4.2
+
+SELECT * INTO dbo.Curso6 
+FROM dbo.Curso 
+WHERE 1=2; 
+GO
+
+select * from dbo.Curso6 ;
+
+INSERT INTO dbo.Curso6 
+SELECT a.* 
+FROM OPENROWSET( 
+	BULK 'g:\bcp\Curso_A.dat', 
+	FORMATFILE = 'g:\bcp\Curso.fmt'
+	) AS a;
+
+select * from dbo.Curso6 ;
+GO
+
+
+-- Consultas Distribuidas
+
+sp_configure;
+GO
+
+sp_configure 'show advanced options', 1; 
+RECONFIGURE; 
+go
+
+sp_configure; 
+GO
+
+sp_configure 'Ad Hoc Distributed Queries', 1; 
+RECONFIGURE; 
+GO
+
+sp_configure; 
+GO
+
+SELECT a.* 
+FROM OPENROWSET('SQLNCLI', 
+'Server=192.168.40.30;Trusted_Connection=yes', 
+'SELECT * FROM EDUTEC.dbo.curso') AS a;
+
+
+SELECT a.* 
+FROM OPENROWSET('SQLNCLI', 
+'Server=192.168.40.26;Trusted_Connection=yes', 
+'SELECT * FROM EDUTEC.dbo.curso') AS a;
+
+
+-- Ejercicio
+/*
+Hacer un ejercicio quemuestre los cursos
+programados de 3 bases de datos, el 
+ciclo 2019-12.
+
+SERVIDOR  IDCURSOPROG  NOMBRE-CURSO   NOMBRE-PROFESOR
+--------------------------------------------------------
+
+*/
+
+
+SELECT 'LOCAL',CP.IdCursoProg, C.NomCurso, P.NomProfesor
+FROM EDUTEC.DBO.CURSO C
+JOIN EDUTEC.DBO.CursoProgramado CP
+ON C.IdCurso = CP.IdCurso
+LEFT JOIN EDUTEC.DBO.Profesor P
+ON CP.IdProfesor = P.IdProfesor
+WHERE CP.IdCiclo = '2019-12'
+UNION
+SELECT 'BRAVO', A.IdCursoProg, A.NomCurso, A.NomProfesor 
+FROM OPENROWSET('SQLNCLI', 
+'Server=192.168.40.26;Trusted_Connection=yes', 
+'SELECT CP.IdCursoProg, C.NomCurso, P.NomProfesor
+FROM EDUTEC.DBO.CURSO C
+JOIN EDUTEC.DBO.CursoProgramado CP
+ON C.IdCurso = CP.IdCurso
+LEFT JOIN EDUTEC.DBO.Profesor P
+ON CP.IdProfesor = P.IdProfesor
+WHERE CP.IdCiclo = ''2019-12''') AS A
+GO
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
